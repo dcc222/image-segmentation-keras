3cbf4f8f3e891572d22df2e9de854500c5747e00
杜晨琛 <50733870+dcc222@users.noreply.github.com>
2019-07-17 11:04:25 +0800

    Update metrics.py

--- keras_segmentation/metrics.py
+++ keras_segmentation/metrics.py
@@ -4,7 +4,7 @@ EPS = 1e-12
 
 def get_iou( gt , pr , n_classes ):
 	class_wise = np.zeros(n_classes)
-	for cl in range(n_classes):
+	for cl in range(n_classes):#cl为类别class,gt为ground truth为真实标签类别，pr为predict预测的类别
 		intersection = np.sum(( gt == cl )*( pr == cl ))
 		union = np.sum(np.maximum( ( gt == cl ) , ( pr == cl ) ))
 		iou = float(intersection)/( union + EPS )
